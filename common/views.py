a4107b4d7c6648364a28c694ed1fb8aa434f2b56
Daniel Quintero <quinterod164@gmail.com>
2023-10-23 10:55:46 -0500

    feat: :sparkles: add folium map for landing page

--- common/views.py
+++ common/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+import folium
 
 def index(request):
-    return render(request, "index.html")
+    m = folium.Map(location=[4.142, -73.62664], zoom_start=13)
+    context = {'map': m._repr_html_()}
+    return render(request, "index.html", context)
 
 def login(request):
     return render(request, "login.html")
