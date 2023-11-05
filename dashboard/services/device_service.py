685277fd0ca5dccdfc4d22ee63514a7474d6bbb9
Daniel Quintero <quinterod164@gmail.com>
2023-11-05 16:05:08 -0500

    Add get_user_devices method to DeviceService

--- dashboard/services/device_service.py
+++ dashboard/services/device_service.py
@@ -1,3 +1,9 @@
+from dashboard.models import Device
+
+
 class DeviceService:
     def __init__(self):
         print("DeviceService init... 🚀")
+        
+    def get_user_devices(self, user):
+        return Device.objects.filter(user=user)
