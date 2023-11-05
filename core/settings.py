03d3bcbd735f690e55d992ea8340dd0553569ee9
Daniel Quintero <quinterod164@gmail.com>
2023-11-05 18:12:04 -0500

    Add django.contrib.humanize to INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # third party
     "cloudinary",
     "cloudinary_storage",
