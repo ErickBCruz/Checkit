5e0f04bd08fdae2d2597f4859f0b7821f8a84cc3
Daniel Quintero <quinterod164@gmail.com>
2023-11-05 19:01:15 -0500

    Add device registration modal and form to dashboard-devices.html and CreateDeviceForm to dashboard_devices_views.py

--- dashboard/views/dashboard_devices_views.py
+++ dashboard/views/dashboard_devices_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
+
+from dashboard.forms.device.create_divice_form import CreateDeviceForm
 
 from dashboard.services.device_service import DeviceService
 from users.services.client_service import ClientService
@@ -16,13 +17,16 @@ def devices_view(request):
     
     is_client = client_service.is_client(request.user)
     devices = None
+    device_form = None
 
     if is_client:
         client = client_service.get_client(request.user)
         devices = device_service.get_paged_user_devices(client, page_number, PER_PAGE)
-
+        device_form = CreateDeviceForm()
+    
     context = {
         "devices": devices,
+        "device_form": device_form,
     }
 
     return render(request, "dashboard-devices.html", context)
