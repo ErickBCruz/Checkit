6f16566ca6f576de9daff126e35b8decd16f75d2
Daniel Quintero <quinterod164@gmail.com>
2023-11-06 17:51:27 -0500

    Add EnterpriseFollowers and EnterpriseViews to admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, Client, Enterprise
+from .models import User, Client, Enterprise, EnterpiseFollowers, EnterpriseViews
 
 admin.site.register(User)
 admin.site.register(Client)
 admin.site.register(Enterprise)
+admin.site.register(EnterpiseFollowers)
+admin.site.register(EnterpriseViews)
