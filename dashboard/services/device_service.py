67314871cdb2353f3f3c1b6a2a6d58a954fbe79f
Daniel Quintero <quinterod164@gmail.com>
2023-11-07 02:14:39 -0500

    Add maintenance status to user devices

--- dashboard/services/device_service.py
+++ dashboard/services/device_service.py
@@ -1,5 +1,7 @@
 from dashboard.models import Device
 from django.core.paginator import Paginator
+from dashboard.services.maintenance_service import MaintenanceService
+
 
 class DeviceService:
     def __init__(self):
@@ -10,5 +12,12 @@ class DeviceService:
 
     def get_paged_user_devices(self, user, page, per_page):
         devices = self.get_user_devices(user)
+        
+        for device in devices:
+            status = device.maintenance = MaintenanceService().get_status_from_device(device)
+            if not status or status == None:
+                device.status = "-1"
+            else:
+                device.status = str(status)
         paginator = Paginator(devices, per_page)
         return paginator.get_page(page)
--- dashboard/services/maintenance_service.py
+++ dashboard/services/maintenance_service.py
@@ -1,15 +1,30 @@
 import uuid
 from dashboard.models import DeviceMaintenance
+from django.core.paginator import Paginator
 
 
-class MaintenanceService():
+class MaintenanceService:
     def __init__(self) -> None:
         pass
-    
+
     def generate_random_ticket(self) -> str:
         ticket = uuid.uuid4().hex[:8].upper()
         return ticket
-    
+
     def get_mainenance_status_by_ticket(self, ticket: str) -> str:
         maintenance = DeviceMaintenance.objects.get(ticket=ticket)
         return maintenance
+    
+    def get_status_from_device(self, device) -> str:
+        maintenance = DeviceMaintenance.objects.filter(device=device).first()
+        return maintenance.status if maintenance else None
+
+    def get_maintenance_enterprise_devices(self, enterprise) -> list:
+        devices = DeviceMaintenance.objects.filter(enterprise=enterprise)
+        return devices
+    
+    def get_paged_maintenance_enterprise_devices(self, enterprise, page, per_page):
+        devices = self.get_maintenance_enterprise_devices(enterprise)
+        paginator = Paginator(devices, per_page)
+        return paginator.get_page(page)
+        
